a0a37f26d165e42cf49699b1b8588287abcab799
Agunq <agunq.e@gmail.com>
2021-02-19 19:38:29 +0700

    update tes
    make it more easy

--- tes.py
+++ tes.py
@@ -1,4 +1,11 @@
 from gt import GT
 
 tes = GT()
-print(tes.t("selamat pagi", "id", "ja"))
+print(tes.t("selamat pagi", "auto", "ja"))
+
+#another example
+text = tes.text("good morning", "en", "ko")
+print(text["translate"])
+
+text = tes.text("좋은 아침", "auto", "en")
+print(text["translate"])
