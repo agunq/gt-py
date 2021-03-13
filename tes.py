091031ff70880d5dd5170a3e8ff8e441e4d3e90b
Agunq <agunq.e@gmail.com>
2021-03-13 20:28:31 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,11 +1,11 @@
 from gt import GT
 
 tes = GT()
-print(tes.t("selamat pagi", "auto", "ja"))
+print(tes.t("selamat pagi", "ja", "auto"))
 
 #another example
-text = tes.text("good morning", "en", "ko")
+text = tes.text("good morning", "ko", "en")
 print(text["translate"])
 
-text = tes.text("좋은 아침", "auto", "en")
+text = tes.text("좋은 아침", "en")
 print(text["translate"])
